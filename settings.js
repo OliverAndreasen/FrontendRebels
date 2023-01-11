15cbba5b018ef95434823d89917f9684b2c462bc
Oliver Andreasen <90641158+OliverAndreasen@users.noreply.github.com>
2023-01-11 16:57:00 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const ip = "http://localhost:8080/"
+const ip = "https://eksamenoliver.azurewebsites.net"
 export const matchesUrl = ip + "api/matches/";
 export const teamsUrl = ip + "api/teams/";
 export const clubUrl = ip + "api/clubs/"
